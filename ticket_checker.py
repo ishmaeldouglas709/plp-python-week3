03a5f36e99d8e6ae6c84962c40e7897be2020e70
ishmaeldouglas709 <ishmaeldouglas709@gmail.com>
2026-09-01 16:42:10 +0300

    Update ticket_checker.py

--- ticket_checker.py
+++ ticket_checker.py
@@ -1,1 +1,16 @@
+#TICKET CHECKER, CHILDREN PAY CHILDREN ALLOCATED PRICE WHILE ADULTS PAY FULL PRICE
 
+age=int(input("What is your age:"))
+is_adult = age >= 18
+
+ticket_price = {
+    "adult":1500,"children":500
+}
+
+if is_adult :
+    #pays adult price
+    print(f'The ticket price is: {ticket_price["adult"]}')
+else :
+    #pays child price
+    print(f'The ticket price is: {ticket_price["children"]}')
+    
