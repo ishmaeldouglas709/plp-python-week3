ff49a935cf97bf3af6ffbf4c3e3f93f4a734be08
ishmaeldouglas709 <ishmaeldouglas709@gmail.com>
2026-09-01 16:08:10 +0300

    Update name_greeter.py

--- name_greeter.py
+++ name_greeter.py
@@ -1,1 +1,17 @@
+# we need to greet by first name, then we test as such: 
+#  1. i the user having 2 or more names? greet by first name else prompt for second name
 
+
+name=input('Input your name:')
+
+processed_name = name.split()
+
+if len(processed_name) > 1 :
+    #greeting by first name
+    print(f'Greetings : {processed_name[0]}')
+    
+else :
+    corrected_name=input('Please input more than one name:')
+
+
+        
