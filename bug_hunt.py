64f36654fa75eaf4b0fddd14138d91a54694557b
ishmaeldouglas709 <ishmaeldouglas709@gmail.com>
2026-09-01 16:18:40 +0300

    Update bug_hunt.py

--- bug_hunt.py
+++ bug_hunt.py
@@ -1,1 +1,15 @@
+# fixed the syntax error by adding closing quotation marks:
+print("Welcome to the Bug Hunt!")
+name = input("What is your name? ")
+
+# fixed the syntax error by referencing the variable name correctly using print(f'')
+# also fixed the semantics error by correcting spellings for var name 
+print(f"Nice to meet you, {name}")
+
+#fixed it by changing return  var from type str to int
+age = int(input("How old are you? "))
+
+#fixed the syntx error by enablig proper variable manipultion oof adding numbers not concacting strings
+print(f"Next year you will be : {age + 1}")
+
 
